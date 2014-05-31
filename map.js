dbbb8084294a04b091adfdd58505dde886c0ee44
Mark Phillips <embeepea@git>
2014-05-31 12:18:27 -0400

    get basic map working, add bus routes geojson file (not used yet)

--- map.js
+++ map.js
@@ -7,19 +7,23 @@ $(document).ready(function() {
     }).setView([35.596, -82.55], 14);
 
 
+/*
     var cloudmadeBaseLayer = L.tileLayer("http://{s}.tile.cloudmade.com/" + cloudmade_api_key + "/997/256/{z}/{x}/{y}.png", {
         //            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
     });
+*/
 
     var osmBaseLayer = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 18
     }).addTo(map);
 
 
+/*
     var layerControl = L.control.layers({
         'Open Street Map' : osmBaseLayer,
         'Cloudmade' : cloudmadeBaseLayer
     }).addTo(map);
-    
+*/
+  
 });
